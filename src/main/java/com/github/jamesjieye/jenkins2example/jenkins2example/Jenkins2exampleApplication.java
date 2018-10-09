8d5904fd5ef33bd5176acd853834fecc95f8b19d
James J. Ye <james.ye@hotmail.com>
2018-10-09 18:57:49 +0800

    Add Spring Boot app starter

---
+++ src/main/java/com/github/jamesjieye/jenkins2example/jenkins2example/Jenkins2exampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.jamesjieye.jenkins2example.jenkins2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins2exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins2exampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/jamesjieye/jenkins2example/jenkins2example/Jenkins2exampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.jamesjieye.jenkins2example.jenkins2example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jenkins2exampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
